6a3ac04ea6b51bc079db74bb61030e388d960291
Swetha-N169 <swethanandigama@gmail.com>
2026-04-22 17:07:10 +0530

    commit and push

---
+++ network module/main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "main" {
+  name        = "roboshop-allow-all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "roboshop-allow-all"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
